f2fc6f9e64b9ea15384bbc2c43f5a57f05eeab9b
Mdletshe <s.mdletshe@marval.co.uk>
2022-01-15 02:08:12 +0200

    Read images from api

--- src/pages/UserLists.js
+++ src/pages/UserLists.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import Users from './Users'
 import axios from 'axios'
 
 function UserLists() {
     var userUrl ='http://localhost:55147/api/users/';
     var accountUrl = 'http://localhost:55147/api/accounts/'
+
+    const [userList, setUserList] = useState([])
+
+    useEffect(() => {
+        refreshUserList();
+    }, [])
+
     const userAPI = () => {
         return {
             fetchAll: () => axios.get(userUrl +'getusers'),
@@ -14,16 +21,43 @@ function UserLists() {
         }
     }
 
+    function refreshUserList()  {
+        userAPI().fetchAll()
+            .then(response => setUserList(response.data.data))
+            .catch(error => console.log(error))
+    }
+
+    // function refreshUserList()  {
+    //     userAPI().fetchAll()
+    //     .then(response => {
+    //         var rd = response.data.data;
+    //         //onSuccess();
+    //     })
+    //         .catch(error => {
+    //             console.log(error)
+    //         })
+    // }
+
     const addOrEdit = (formData, onSuccess) => {
         userAPI().create(formData)
             .then(response => {
                 onSuccess();
+                refreshUserList();
             })
             .catch(error => {
                 console.log(error)
             })
     }
 
+    const imageCard = data => (
+        <div className='card'  >
+            <img src={data.userImageSrc} className='card-img-top rounded-circle'/>
+            <div className='card-body'>
+                <h5 className='card-title'>{data.userName}</h5>
+                <span className='card-text'>{data.id}</span>
+            </div>
+        </div>
+    )
     return (
         <>
             <div className='container'>
@@ -39,7 +73,19 @@ function UserLists() {
                         <Users addOrEdit={addOrEdit} />
                     </div>
                     <div className='col-md-8'>
-                        <div>List of Users</div>
+                        <table>
+                            <tbody>
+                                {
+                                     [...Array(Math.ceil(userList.length/3))].map((e,i) =>
+                                     <tr key={i}>
+                                         <td>{ imageCard(userList[3 * i])}</td>
+                                         <td>{userList[3 * i + 1] ? imageCard(userList[3 * i + 1]) : null}</td>
+                                         <td>{userList[3 * i + 2] ? imageCard(userList[3 * i + 2]) : null}</td>
+                                     </tr>
+                                     )
+                                }
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
